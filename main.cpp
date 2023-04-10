fe8a493413cf09cfb5baf74909ed0ef47f3b403f
mac <mac@macs-MacBook-Pro.local>
2023-04-10 18:27:26 +0300

    Timer_2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+void print(const boost::system::error_code& /*e*/)
+{
+  std::cout << "Hello, world!" << std::endl;
+}
+
 int main()
 {
   boost::asio::io_context io;
 
   boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-  t.wait();
-  std::cout << "Hello, world!" << std::endl;
+  t.async_wait(&print);
+  io.run();
 
   return 0;
 }
