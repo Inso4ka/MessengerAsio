9416236454b90ef29496e30906637b65e66e7971
mac <mac@macs-MacBook-Pro.local>
2023-04-12 16:01:00 +0300

    Synchronous messenger

---
+++ client.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <thread>
+
+using boost::asio::ip::tcp;
+
+void readMessage(tcp::socket& socket)
+{
+    try
+    {
+        while (true)
+        {
+            // читаем сообщение от сервера
+            char buf[1024];
+            boost::system::error_code error;
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof) // обрабатываем случай отсоединения от сервера
+            {
+                std::cout << "Server disconnected." << std::endl;
+                break; // выходим из цикла чтения сообщений
+            }
+            else if (error) // обрабатываем другие ошибки
+            {
+                std::cerr << "Error reading from server: " << error.message() << std::endl;
+                break; // выходим из цикла чтения сообщений
+            }
+
+            // выводим сообщение на экран
+            std::cout << "Received message: " << std::string(buf, len) << std::endl;
+        }
+    }
+    catch (std::exception& e) // обрабатываем исключения
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 3)
+        {
+            std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(argv[1], argv[2]);
+        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+        tcp::socket socket(io_service);
+        boost::asio::connect(socket, endpoint_iterator);
+
+        std::cout << "Connected to " << argv[1] << ":" << argv[2] << std::endl;
+
+        std::thread t([&socket]{ readMessage(socket); }); // запускаем новый поток для чтения сообщений от сервера
+
+       while (true)
+        {
+            try
+            {
+                // читаем введенную пользователем команду
+                std::string command;
+                std::getline(std::cin, command);
+
+                // проверяем, является ли команда командой отправки сообщения
+                if (command.substr(0, 4) == "send")
+                {
+                    // извлекаем идентификатор получателя и текст сообщения из команды
+                    std::string recipientId;
+                    std::string message;
+                    size_t spacePosition = command.find_first_of(" ");
+                    if (spacePosition != std::string::npos)
+                    {
+                        recipientId = command.substr(spacePosition + 1);
+                        size_t messageStartPosition = recipientId.find_first_of(" ") + 1;
+                        message = recipientId.substr(messageStartPosition);
+                        recipientId = recipientId.substr(0, messageStartPosition - 1);
+
+                        // отправляем сообщение указанному получателю через сокет
+                        std::string fullMessage = "send " + recipientId + " " + message;
+                        boost::asio::write(socket, boost::asio::buffer(fullMessage));
+                    }
+                    else
+                    {
+                        std::cerr << "Invalid send command format." << std::endl;
+                    }
+                }
+                else
+                {
+                    // отправляем команду серверу через сокет
+                    boost::asio::write(socket, boost::asio::buffer(command + "\n"));
+                }
+            }
+            catch (std::exception& e) // обрабатываем исключения
+            {
+                std::cerr << "Exception caught: " << e.what() << std::endl;
+                break; // выходим из цикла отправки команд
+            }
+        }
+
+        // останавливаем поток чтения сообщений и закрываем сокет
+        t.join();
+        socket.close();
+    }
+    catch (std::exception& e) // обрабатываем исключения
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <vector>
+#include <map>
+#include <thread>
+#include <memory>
+
+using boost::asio::ip::tcp;
+
+std::map<std::string, tcp::socket*> clientSockets; // map для хранения идентификаторов клиентов и соответствующих им сокетов
+std::vector<std::thread> threads; // вектор для хранения потоков
+
+void sendMessage(const std::string& message, const std::string& recipientId)
+{
+    std::cout << "Sending message \"" << message << "\" to " << recipientId << std::endl;
+
+    auto it = clientSockets.find(recipientId); // получаем итератор на элемент с указанным идентификатором
+
+    if (it != clientSockets.end() && it->second->is_open()) // если сокет получателя найден и открыт
+    {
+        // отправляем сообщение через сокет
+        boost::asio::write(*it->second, boost::asio::buffer(message));
+    }
+    else // иначе выдаем ошибку
+    {
+        std::cerr << "Recipient " << recipientId << " not found!" << std::endl;
+        clientSockets.erase(it); // удаляем запись из map
+    }
+}
+
+
+void handleConnection(std::shared_ptr<tcp::socket> socketPtr)
+{
+    std::string clientId = socketPtr->remote_endpoint().address().to_string(); // получаем идентификатор клиента на основе его IP-адреса и порта
+
+    if (!socketPtr->is_open()) {
+        std::cerr << "Socket for client " << clientId << " is not open." << std::endl;
+        return;
+    }
+
+    std::cout << "New connection from " << clientId << std::endl;
+
+    clientSockets[clientId] = socketPtr.get(); // добавляем сокет клиента в map
+
+    while (true)
+    {
+        try
+        {
+            // читаем сообщение от клиента
+            char buf[1024];
+            boost::system::error_code error;
+            size_t len = socketPtr->read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof || error == boost::asio::error::connection_reset) {
+                std::cout << "Client " << clientId << " disconnected." << std::endl;
+                break; // выходим из цикла чтения сообщений от клиента
+            }
+            else if (error) {
+                std::cerr << "Error reading from client " << clientId << ": " << error.message() << std::endl;
+                break; // выходим из цикла чтения сообщений от клиента
+            }
+
+            // разбираем сообщение, чтобы понять, что нужно сделать с сообщением (отправить его получателю или нет)
+            std::istringstream iss(std::string(buf, len));
+            std::string command;
+            iss >> command;
+
+            // если клиент отправляет сообщение конкретному получателю
+            if (command == "send")
+            {
+                std::string recipientId;
+                iss.ignore();
+                std::getline(iss, recipientId, ' '); // считываем идентификатор получателя
+
+                tcp::socket* recipientSocket = clientSockets[recipientId];
+                if (recipientSocket) // если получатель найден, отправляем ему сообщение
+                {
+                    std::string message = clientId + ": " + iss.str().substr(5 + recipientId.length()); // формируем новое сообщение с идентификатором отправителя
+                    sendMessage(message, recipientId);
+                } 
+                else // если получатель не найден, отправляем сообщение об ошибке
+                {
+                    std::string errorMessage = "Error: client '" + recipientId + "' not found.";
+                    boost::asio::write(*socketPtr, boost::asio::buffer(errorMessage));
+                }
+            }
+            // если клиент отправляет общее сообщение
+            else
+            {
+                std::cerr << "Unknown command: " << command << std::endl;
+            }
+
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << "Exception caught in handleConnection(): " << e.what() << std::endl;
+        }
+    }
+
+    // удаляем сокет клиента из map
+    clientSockets.erase(clientId);
+}
+
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+
+        std::cout << "Server started listening on " << std::atoi(argv[1]) << std::endl;
+
+        for (;;)
+        {
+            // создаем сокет для нового подключения
+            std::shared_ptr<tcp::socket> socketPtr = std::make_shared<tcp::socket>(io_service);
+
+            // ждем новых подключений
+            acceptor.accept(*socketPtr);
+
+            // запускаем функцию handleConnection() в новом потоке для обработки подключения
+            std::thread th(handleConnection, socketPtr);
+            th.detach();
+
+
+            threads.emplace_back(std::move(th)); // добавляем поток в вектор потоков
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    for (auto& t : threads) // ожидаем завершения всех потоков
+    {
+        t.join();
+    }
+
+    return 0;
+}
