3d740aefb0e79e448e1cb6ac0dd2ea88b34d64e5
mac <mac@macs-MacBook-Pro.local>
2023-05-09 23:21:52 +0300

    Fixed errors // added cmake to client & server

--- server/database.h
+++ server/database.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <boost/asio.hpp>
 #include <sqlite3.h>
 #include <boost/random.hpp>
--- server/server.h
+++ server/server.h
@@ -1,3 +1,5 @@
+#pragma once 
+
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
